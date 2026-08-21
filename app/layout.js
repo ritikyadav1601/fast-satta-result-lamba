b181f4d1b18e24aef7e1f17df9888d821457483c
Ritik yadav <rythukran713@gmail.com>
2026-08-21 12:06:37 +0530

    added verificaation

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css';
 
-export const metadata = { metadataBase:new URL('https://www.fast-satta-result.com'), title:'Fast Satta Result', description:'Fast result updates and historical charts.', applicationName:'Fast Satta Result', authors:[{name:'Fast Satta Result',url:'https://www.fast-satta-result.com'}], creator:'Fast Satta Result', publisher:'Fast Satta Result', robots:{index:true,follow:true},formatDetection:{email:false,address:false,telephone:false},openGraph:{siteName:'Fast Satta Result',type:'website',locale:'en_IN',url:'https://www.fast-satta-result.com'},twitter:{card:'summary'} };
+export const metadata = { metadataBase:new URL('https://www.fast-satta-result.com'), title:'Fast Satta Result', description:'Fast result updates and historical charts.', applicationName:'Fast Satta Result', authors:[{name:'Fast Satta Result',url:'https://www.fast-satta-result.com'}], creator:'Fast Satta Result', publisher:'Fast Satta Result', verification:{google:'UOK6xgFbN-7ssk5eEYZ73sSs4tkg4UYKcVwt2ghBGJo'}, robots:{index:true,follow:true},formatDetection:{email:false,address:false,telephone:false},openGraph:{siteName:'Fast Satta Result',type:'website',locale:'en_IN',url:'https://www.fast-satta-result.com'},twitter:{card:'summary'} };
 
 export default function RootLayout({ children }) {
   return <html lang="en"><body>{children}</body></html>;
